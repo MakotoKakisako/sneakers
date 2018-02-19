1366b5787632d0911df5ee1714d2ba0d3cbb80ec
m-kakisako <m-kakisako@froide.co.jp>
2018-02-19 16:58:43 +0900

    :arrow_up: version up to 4.0.0

--- lib/sneakers/publisher.rb
+++ lib/sneakers/publisher.rb
@@ -12,7 +12,7 @@ module Sneakers
       to_queue = options.delete(:to_queue)
       options[:routing_key] ||= to_queue
       Sneakers.logger.info {"publishing <#{msg}> to [#{options[:routing_key]}]"}
-      Sneakers.thread_logger.info "■■■■■■■■■■■■■■■■■■■■"
+      Sneakers.logger.info "■■■■■■■■■■■■■■■■■■■■"
       @exchange.publish(msg, options)
     end
 
--- lib/sneakers/version.rb
+++ lib/sneakers/version.rb
@@ -1,3 +1,3 @@
 module Sneakers
-  VERSION = "2.3.2"
+  VERSION = "4.0.1"
 end
